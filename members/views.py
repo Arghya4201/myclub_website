74a57f91f083996a542f6db441ddb4fe2450a34c
Arghya Bhowmick <arghyabhowmick21@gmail.com>
2024-09-10 12:09:43 +0530

    Djando Wednedays 24 register users

--- members/views.py
+++ members/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import login, authenticate, logout
 from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
+
 
 
 def login_user(request):
@@ -11,6 +13,7 @@ def login_user(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
+            messages.success(request, 'You have been logged in!')
             return redirect('home')
         else:
             messages.error(request, 'Username or password is incorrect')
@@ -22,4 +25,19 @@ def logout_user(request):
     logout(request)
     messages.success(request, 'You have been logged out!')
     return redirect('home')
+
+def register_user(request):
+    form  = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            messages.success(request, 'You have successfully registered')
+            return redirect('home')
+        else:
+            form = UserCreationForm()
+    return render(request, 'authenticate/register_user.html', {'form': form})
         
